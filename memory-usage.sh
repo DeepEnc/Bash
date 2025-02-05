c1bd559358decc34f3fc3a69a209eda4718970dc
deepenc <sunilojha525@gmail.com>
2025-02-05 18:24:22 +0545

    add another memory usage script

--- memory-usage.sh
+++ memory-usage.sh
@@ -1,3 +1,4 @@
+# #!/bin/bash
 THRESHOLD=2
 USAGE=$(free -g | awk '/^Mem:/ {print $3}')
 
@@ -6,3 +7,15 @@ if [ "$USAGE" -gt "$THRESHOLD" ]; then
 else
     echo "Memory Usage is fine. Current Memory Usage: $USAGE"
 fi
+
+
+#!/bin/bash
+THRESHOLD=2
+USAGE=$(free -g | awk 'NR==2 {print $3}')
+
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+    echo "$USAGE is greater than $THRESHOLD"
+else
+    echo "Memory Usage is fine. Current Memory Usage: $USAGE"
+fi
+
